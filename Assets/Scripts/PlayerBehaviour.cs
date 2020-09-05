71f5dae6a02965ed2a59a52a41fe07c63a7637e0
Rodrigo Petters <rodrigofmp@gmail.com>
2020-09-05 19:24:21 -0300

    Assets / movimento do player

---
+++ Assets/Scripts/PlayerBehaviour.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerBehaviour : MonoBehaviour
+{
+    private Rigidbody2D rb;
+    public float Speed = 4f;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void FixedUpdate()
+    {
+        Vector2 dir = Vector2.zero;
+
+        if (Input.GetKey(KeyCode.W))
+        {
+            dir.y = 1;
+        }
+        else if (Input.GetKey(KeyCode.S))
+        {
+            dir.y = -1;
+        }
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            dir.x = -1;
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            dir.x = 1;
+        }
+
+        rb.velocity = dir * Speed;
+    }
+}
